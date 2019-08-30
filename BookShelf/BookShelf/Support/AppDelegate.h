1058fd86506ec07c2049ab7f90d3ce9986764ec4
NohEunTae <dmsxoeldk@naver.com>
2019-08-30 22:07:01 +0900

    New Book #3
    TabBarController 설정
    
    New, Search View는 Detail View로 전환되는 과정이 있기 떄문에 각각의 NavigationController를 설정

---
+++ BookShelf/BookShelf/Support/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookShelf
+//
+//  Created by user on 30/08/2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
