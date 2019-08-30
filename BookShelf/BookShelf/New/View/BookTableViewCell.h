3fc2888b02d5538e8ce9b60f1c0e9a29112687c4
NohEunTae <dmsxoeldk@naver.com>
2019-08-30 22:03:15 +0900

    New Book #3
    Table View Cell
    
    configure 메소드를 통해 뷰를 동기화

---
+++ BookShelf/BookShelf/New/View/BookTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  BookTableViewCell.h
+//  BookShelf
+//
+//  Created by user on 30/08/2019.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Book.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BookTableViewCell : UITableViewCell
+
+- (void)configure:(Book *)book;
+
+@end
+
+NS_ASSUME_NONNULL_END
